a72c1b92b2aa40bf56e90842f1d269c860f9e1d5
Aman Umrao <amanumrao28@gmail.com>
2026-07-24 09:29:24 +0530

    Remove all project files from main

--- server.js
+++
@@ -1,9 +0,0 @@
-const app=require("./src/app");
-require("dotenv").config();
-const connectDB=require("./src/db/db");
-connectDB();
-
-app.listen(3000,()=>{
-    console.log("server is running on port 3000");
-})
-// express validator jest and supertest are used for testing the api endpoints
